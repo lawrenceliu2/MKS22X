7d970507defa747b7a884866e1b244dee4c0bff1
Lawrence Liu <lawrence255@yahoo.com>
2016-02-08 23:40:20 -0500

    Tried to fix up the errors, made the function more precise.

--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -6,11 +6,15 @@ public class Recursion{
 
     public double sqrt (double n){
 	double guess = 1;
-	return helper(n,guess);
+	if (n==0){
+	    return 0;
+	}else{
+	    return helper(n,guess);
+	}
     }
 
     public static double helper (double n, double guess){
-	if (Math.abs(guess * guess - n)  < 0.000001){
+	if (Math.abs(guess * guess - n)  < 0.00000000001){
 	    return guess;
 	}else{
 	    guess = n / (guess * 2) + (guess / 2);
