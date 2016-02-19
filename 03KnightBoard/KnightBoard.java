7d6f31373c11661efb7a7ad5c5b9e634a302dd30
Lawrence Liu <lawrence255@yahoo.com>
2016-02-19 18:19:19 -0500

    Finished printSolution and KnightBoard in the revised way.

--- 03KnightBoard/KnightBoard.java
+++ 03KnightBoard/KnightBoard.java
@@ -1,31 +1,26 @@
 public class KnightBoard{
     private int[][]board;
 
-    public KnightBoard (int n){
-	board = new int[n][n];
+    public KnightBoard (int size){
+	board = new int[size][size];
     }
 
     public void printSolution (){
 	for (int i=0;i<board.length;i++){
 	    for (int k=0;k<board[i].length;k++){
-		System.out.print(board[i][k]+" ");
+		if (board.length*board.length>=10){
+		    if (board[i][k]<10){
+			System.out.print(" "+board[i][k]+" ");
+		    }
+		}else{
+		    System.out.print(board[i][k]+" ");
+		}
 	    }
 	    System.out.print("\n");
 	}
     }
 
-    public boolean move (int row, int col){
-	if (board[row-2][col-1]!=0&&
-	    board[row-1][col-2]!=0){
-	    return false;
-	}
-    }
-
     public boolean solve (){
 	
-    }
-
-    public boolean helper(int col){
-
     }
 }
