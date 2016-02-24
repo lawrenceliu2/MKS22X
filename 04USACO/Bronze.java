9719164483432f201bf1cc6adf7c2e7b25abb399
Lawrence Liu <lawrence255@yahoo.com>
2016-02-24 13:01:48 -0500

    Started Maze

--- 04USACO/Bronze.java
+++ 04USACO/Bronze.java
@@ -17,22 +17,22 @@ public class Bronze{
     }
 
     
-    public static void main(String[]args){
+    public static void solve(){
 	try{
-	Scanner s = new Scanner(new File("makelane.in"));
-	int rows = s.nextInt();
-	int cols = s.nextInt();
-	int elevation = s.nextInt();
-	int instructions = s.nextInt();
-	Bronze lake = new Bronze(rows, cols);
+	    Scanner s = new Scanner(new File("makelake.in"));
+	    int rows = s.nextInt();
+	    int cols = s.nextInt();
+	    int elevation = s.nextInt();
+	    int instructions = s.nextInt();
+	    Bronze lake = new Bronze(rows, cols);
 	
-	for (int i = 0;i<rows;i++){
-	    for (int k = 0;k<cols;k++){
-		lake.add(i,k,s.nextInt());
+	    for (int i = 0;i<rows;i++){
+		for (int k = 0;k<cols;k++){
+		    lake.add(i,k,s.nextInt());
+		}
 	    }
-	}
 
-        for (int i=0;i<instructions;i++){
+	    for (int i=0;i<instructions;i++){
 	        int row=s.nextInt()-1;
 		int col=s.nextInt()-1;
 		int inches=s.nextInt();
@@ -47,31 +47,35 @@ public class Bronze{
 	    }
 
 
-	for (int i = 0;i<rows;i++){
-	    for (int k = 0;k<cols;k++){
-		if (lake.getHeight(i,k)<elevation){
-		    lake.add(i,k,elevation-lake.getHeight(i,k));
-		}else{
-		    lake.add(i,k,0);
+	    for (int i = 0;i<rows;i++){
+		for (int k = 0;k<cols;k++){
+		    if (lake.getHeight(i,k)<elevation){
+			lake.add(i,k,elevation-lake.getHeight(i,k));
+		    }else{
+			lake.add(i,k,0);
+		    }
 		}
 	    }
-	}
 
-	int total=0;
-	for (int i = 0;i<rows;i++){
-	    for (int k = 0;k<cols;k++){
-		if (lake.getHeight(i,k)>0){
-		    total+=lake.getHeight(i,k);
+	    int total=0;
+	    for (int i = 0;i<rows;i++){
+		for (int k = 0;k<cols;k++){
+		    if (lake.getHeight(i,k)>0){
+			total+=lake.getHeight(i,k);
+		    }
 		}
 	    }
-	}
-	int volume = total*72*72;
+	    int volume = total*72*72;
 
-	System.out.println(volume+", 7, Liu, Lawrence");
+	    System.out.println(volume+",7,Liu,Lawrence");
 	}
 	
 	catch (FileNotFoundException e){
 	    System.out.println(e);
 	}
+    }
+
+    public static void main(String[]args){
+	solve();
     }
 }
