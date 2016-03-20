52af299d472f0e9babf1840b1664965869c5ca6e
Lawrence Liu <lawrence255@yahoo.com>
2016-03-20 14:00:33 -0400

    Finished with MyLinkedList

--- 08MyLinkedList/MyLinkedList.java
+++ 08MyLinkedList/MyLinkedList.java
@@ -1,5 +1,7 @@
+import java.util.*;
+
 public class MyLinkedList{
-    private LNode start;
+    private LNode start, end;
     private int size;
 
     public class LNode{
@@ -74,8 +76,17 @@ public class MyLinkedList{
 		return -1;
 	    }
 	}
-	int val= current.getNext().get();
-        current.set(0);
+	int val= current.get();
+        for (int k = i;k<size;k++){
+	    if (current.getNext().getNext()==null){
+		current.set(current.getNext().get());
+		current.setNext(null);
+		return val;
+	    }else{
+		current.set(current.getNext().get());
+		current=current.getNext();
+	    }
+	}
 	return val;
     }
 
@@ -83,30 +94,29 @@ public class MyLinkedList{
 	LNode current = start;
         int i = 0;
 	while (i<index){
-	    if (current.get()==value){
-		for (int k = index;k<size;k++){
-		    int val=current.get();
-		    current.set(value);
-		    value=val;
-		    current=current.getNext();
-		}
-		return true;
-	    }else{
-		current=current.getNext();
-	    }
+	    current=current.getNext();
+	    i++;
 	}
-	return false;
+	for (int k = index;k<size;k++){
+	    int val=current.get();
+	    current.set(value);
+	    value=val;
+	    end = current.getNext();
+	    current=current.getNext();
+	}
+	size++;
+	return true;
     }
 
     public boolean add(int data){
 	if (start==null){
 	    start=new LNode(data);
+	    end = start;
+	    size++;
 	}else{
-	    LNode current = start;
-	    while (current.getNext()!=null){
-		current=current.getNext();
-	    }
+	    LNode current = end;
 	    current.setNext(new LNode(data));
+	    end = current.getNext();
 	    size++;
 	}
 	return true;
@@ -140,15 +150,57 @@ public class MyLinkedList{
 
     public static void main(String[]args){
 	MyLinkedList blah = new MyLinkedList();
+	ArrayList<Integer> blargh = new ArrayList<Integer>();
+	long start, end;
+
+	start = System.currentTimeMillis();
+
+	int k = 0;
+	while(k<100){
+	    blargh.add(k);
+	    k++;
+	}
+	System.out.println(blargh);
+	blargh.add(54);
+	blargh.add(-10);
+	blargh.add(47);
+	blargh.add(4,5);
+	System.out.println(blargh);
+	blargh.set(64,5);
+	System.out.println(blargh);
+	blargh.get(83);
+	System.out.println(blargh);
+	blargh.indexOf(5);
+	System.out.println(blargh);
+	blargh.remove(64);
+	System.out.println(blargh);
+
+	end = System.currentTimeMillis();
+	System.out.println("Time: " + (end-start)/1000.0 + " seconds for ArrayList");
+
+	
+
+	start = System.currentTimeMillis();
+	
 	int i=0;
 	while(i<100){
 	    blah.add(i);
 	    i++;
 	}
+	System.out.println(blah);
 	blah.add(54);
 	blah.add(-10);
 	blah.add(47);
 	System.out.println(blah.add(4,5));
 	System.out.println(blah);
+	blah.set(64, 5);
+	System.out.println(blah);
+	System.out.println(blah.get(83));
+	System.out.println(blah.indexOf(5));
+	System.out.println(blah.remove(64));
+	System.out.println(blah);
+
+	end = System.currentTimeMillis();
+	System.out.println("Time: " + (end-start)/1000.0 + " seconds for MyLinkedList");
     }
 }
