e50c8b3dff6616b1de66089a24bab9bad5c5c672
Lawrence Liu <lawrence255@yahoo.com>
2016-03-22 21:34:11 -0400

    Testing doesn't work for PENULTIMATE?????

--- 08MyLinkedList/MyLinkedList.java
+++ 08MyLinkedList/MyLinkedList.java
@@ -71,22 +71,31 @@ public class MyLinkedList <T>{
 	}else{
 	    LNode current = start;
 	    int i = 0;
+
+	    if (index == 0){
+		T val = current.get();
+		current = current.getNext();
+		start = current;
+		size--;
+		return val;
+	    }
+
 	    while (i<index){
-		if (current.getNext()!=null){
-		    current=current.getNext();
-		    i++;
-		}
+		if (index==size-1){
+		    end = current;
+		    }
+		current=current.getNext();
+		i++;
 	    }
 	    T val= current.get();
-	    for (int k = i;k<size;k++){
-		if (current.getNext()==null){
-		    current=current.getNext();
-		    return val;
-		}else{
-		    current.set(current.getNext().get());
-		    current=current.getNext();
-		}
+
+	    for (int k = i;k<size-1;k++){
+		end = current;
+		current.set(current.getNext().get());
+		current = current.getNext();
 	    }
+
+	    size--;
 	    return val;
 	}
     }
@@ -100,6 +109,13 @@ public class MyLinkedList <T>{
 	    while (i<index){
 		current=current.getNext();
 		i++;
+	    }
+	    if (index==size){
+		current.setNext(new LNode(value));
+		current=current.getNext();
+		end = current;
+		size++;
+		return true;
 	    }
 	    for (int k = index;k<size;k++){
 		T val=current.get();
@@ -152,9 +168,118 @@ public class MyLinkedList <T>{
 	return ans+"]";
     }
 
+    public String toString(boolean thing){
+	String ans = "[";
+	LNode data = start;
+	while (data!=end){
+	    if (data == null){
+		data=data.getNext();
+	    }else{
+		ans+=data.get();
+		ans+=", ";
+		data=data.getNext();
+	    }
+	}
+	ans+=data.get();
+	return ans+"]";
+    }
+
 
     public static void main(String[]args){
-	MyLinkedList <Integer>blah = new MyLinkedList<Integer>();
+	MyLinkedList<String> m = new MyLinkedList<String>();
+        ArrayList<String>    n = new ArrayList<String>();
+
+        for(int i = 0; i < 10; i++){
+            n.add(""+i);
+            m.add(""+i);
+        }
+
+        try{
+            m.add(-1,"oops");
+            System.out.println("\n\nAdd -1 #####################################");
+        }catch(IndexOutOfBoundsException e){
+
+        }
+        try{
+            m.add(12,"oops");
+            System.out.println("\n\n add 12 #####################################");
+        }catch(IndexOutOfBoundsException e){
+
+        }
+        try{
+            m.remove(12);
+            System.out.println("\n\n remove 12 #####################################");
+        }catch(IndexOutOfBoundsException e){
+
+        }
+
+        try{
+            m.set(12,"Fwazzat?!?");
+            System.out.println("\n\n set 12 #####################################");
+        }catch(IndexOutOfBoundsException e){
+
+        }
+
+
+        //replace toString(true) with a debug to string that shows the head/tail
+        System.out.println(m.toString(true));
+        System.out.println(n);
+
+        //test removing from head/tail/middle
+        m.remove(0);
+        n.remove(0);
+        System.out.println(m.toString(true));
+
+        m.remove(2);
+        n.remove(2);
+        System.out.println(m.toString(true));
+
+        m.remove(m.size()-1);
+        n.remove(n.size()-1);
+        System.out.println(m.toString(true));
+        System.out.println(n);
+
+        //test adding to end/start
+        m.add(0,"START");
+        n.add(0,"START");
+        m.add(m.size(),"PENULTIMATE");
+        n.add(n.size(),"PENULTIMATE");
+        System.out.println(m.toString(true));
+        System.out.println(n);
+
+        //test add
+        m.add("Z-END!");
+        n.add("Z-END!");
+        System.out.println(m.toString(true));
+        System.out.println(n);
+
+        //test remove random items:
+        Random rand = new Random(0);
+        for(int i = 0; i < 6000; i++){
+            int op = rand.nextInt(4);
+
+            if(op == 0 || n.size()==0){//ensure never empty
+                n.add(""+i);
+                m.add(""+i);
+            }else if(op == 1 ){
+                int x = rand.nextInt(n.size());
+                n.add(x,""+i);
+                m.add(x,""+i);
+            }else{
+		System.out.println(m);
+		System.out.println(n);
+                int x = rand.nextInt(n.size());
+                if(!n.remove(x).equals(m.remove(x))){
+                    System.out.println("Non matching elements removed\n");
+                    System.exit(1);
+                }
+            }
+        }
+        System.out.println(m.toString(true));
+        System.out.println(n);
+
+	
+	/*MyLinkedList <Integer>blah = new MyLinkedList<Integer>();
 	ArrayList<Integer> blargh = new ArrayList<Integer>();
 	long start, end;
 
@@ -206,6 +331,8 @@ public class MyLinkedList <T>{
 	System.out.println(blah);
 
 	end = System.currentTimeMillis();
-	System.out.println("Time: " + (end-start)/1000.0 + " seconds for MyLinkedList");
+	System.out.println("Time: " + (end-start)/1000.0 + " seconds for MyLinkedList");*/
+
+        
     }
 }
