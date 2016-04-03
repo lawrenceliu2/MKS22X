5a60c6cd11c1d449d596b534bd61d1a0d92b14ec
Lawrence Liu <lawrence255@yahoo.com>
2016-04-03 19:05:37 -0400

    Reworked class to accomodate objects/generics. Working grow and addFirst functions. Grow manipulates the start to be 0 and end to be the size, not sure if that was allowed.

--- 10MyDeque/MyDeque.java
+++ 10MyDeque/MyDeque.java
@@ -1,11 +1,12 @@
 import java.util.*;
 
-public class MyDeque{
-    private Object[] data;
+public class MyDeque<T>{
+    private T[] data;
     private int size, start, end;
 
+    @SuppressWarnings("unchecked")
     public MyDeque(){
-	data = new Object[10];
+	data = (T[]) new Object[10];
     }
 
     public String toString(){
@@ -21,25 +22,67 @@ public class MyDeque{
 	return ans;
     }
 
-    public static int size(){
+    public int size(){
 	return size;
     }
+    public int start(){
+	return start;
+    }
+    public int end(){
+	return end;
+    }
 
-    public static void grow(){
-	Object[] newData = new Object[size()*2];
-	for (int i=0;i<data.length;i++){
-	    newData[i]=data[i];
+    private void grow(){
+	T[] newData = (T[]) new Object[data.length*2];
+	int count=0;
+	for (int i=start;i<data.length;i++){
+	    newData[count]=data[i];
+	    count++;
 	}
+	for (int i=0;i<start;i++){
+	    newData[count]=data[i];
+	    count++;
+	}
+	start=0;
+	end=count-1;
 	data=newData;
     }
 
-    public boolean add(Object value){
-	if (size()==data.length){
+    public void addFirst (T value){
+	if (size==data.length){
 	    grow();
 	}
+	if (end==data.length-1){
+	    end=-1;
+	}
 	data[end+1]=value;
-	end++;
 	size++;
+	if (size==1){
+	    end++;
+	    start=end;
+	}else{
+	    end++;
+	}
     }
 
+
+    public static void main(String[]args){
+	MyDeque<Integer> blah = new MyDeque<Integer>();
+	System.out.println(blah);
+	blah.addFirst(0);
+	blah.addFirst(1);
+	blah.addFirst(2);
+	blah.addFirst(3);
+	blah.addFirst(4);
+	blah.addFirst(5);
+	blah.addFirst(6);
+	blah.addFirst(7);
+	blah.addFirst(8);
+	blah.addFirst(9);
+	blah.addFirst(10);
+	System.out.println(blah);
+	System.out.println(blah.size());
+	System.out.println(blah.start());
+	System.out.println(blah.end());
+    }
 }
