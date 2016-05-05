72b51af6a6de6b22c819e6d1bd16171a0e40a15d
Lawrence Liu <lawrence255@yahoo.com>
2016-05-05 00:06:51 -0400

    I severely regret procrastinating this assignment. I should have done it over the break ahhahaaha. I kinda had to reteach myself the BSTree. Regrets. Lesson Learned. I'm not even sure if this works completely. wow i love midnight

--- 12BSTree/BSTree.java
+++ 12BSTree/BSTree.java
@@ -19,42 +19,71 @@ public class BSTree<T extends Comparable<T>>{
 	public void setLeft(Node value){left=value;}
 
 	public void setRight(Node value){right=value;}
-
-	public void add (T value){
-	    if (left==null){
-		left = new Node(value);
-	    }
-	    else if (right==null){
-		right = new Node(value);
-	    }
-	    else{
-
-	}
-
-	public String toString(){
-	    
-	}
-
-	public boolean contains(T value){
-		
-	}
     }
-
+    
     private Node root;
 
     public int getHeight(){
-	return 0;
+	return getHeight(root);
     }
-
+	
+    public int getHeight(Node data){
+	if (data==null){
+	    return 0;
+	}else{
+	    return 1+Math.max(getHeight(data.getLeft()),getHeight(data.getRight()));
+	}
+    }
+    
     public void add(T value){
-	root.add(value);
+	Node data = new Node(value);
+	if (root==null){
+	    root = data;
+	}else{
+	    Node current = root;
+	    Node parent = null;
+	    parent = current;
+	    if (value.compareTo(current.getData())<0){
+		current = current.getLeft();
+		if (current==null){
+		    parent.setLeft(data);
+		}
+	    }else{
+		current=current.getRight();
+		if (current==null){
+		    parent.setRight(data);
+		}
+	    }
+	}
     }
 
     public boolean contains(T value){
-	return root.contains(value);
+	Node current = root;
+	while (current!= null){
+	    if (current.getData()==value){
+		return true;
+	    }else if (current.getData().compareTo(value)>0){
+		current=current.getLeft();
+	    }else{
+		current=current.getRight();
+	    }
+	}
+	return false;
     }
 
     public String toString(){
-	return root.toString();
+	return toString(root);
+    }
+
+    public String toString (Node data){
+	String ans = "";
+	if (data == null){
+	    return "";
+	}else{
+	    ans+= toString (data.getLeft());
+	    ans+= toString (data.getRight());
+	    ans+= data.getData().toString();
+	    return ans;
+	}
     }
 }
